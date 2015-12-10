5614a767eb1aa1ca67ef27b65d72b974490c597f
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2015-12-10 06:38:50 -0500

    z

---
+++ Projectflask copy/todoapp.py
@@ -0,0 +1,29 @@
+__author__ = 'malcolmbarnes'
+from flask import Flask, render_template, request, redirect
+app= Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def toDo():
+
+    return render_template ('index.html', users=[1,2,3])
+
+
+def hello_world():
+    author= "Malcolm's Page"
+    name= "Malcolm"
+    return render_template('index.html', author= author, name= name, users= [1,2,3])
+
+@app.route('/submit', methods= ['POST'])
+def submit():
+    task= request.form['task']
+    priority= request.form['priority']
+    email= request.form['email']
+    print(task,email,priority)
+    return redirect ('/')
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug= True)
